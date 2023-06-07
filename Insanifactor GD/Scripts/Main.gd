fcfad7092f97364dcd1d1811b93d4da8b3a4a2b7
Codi <71397710+ninjassifi@users.noreply.github.com>
2023-06-07 15:07:45 -0700

    Update 1.07
    Tilemap almost finished
    
    Things include:
    Made function to place tile and configure it automatically
    Made function to place an area of a type of block automatically
    Made the player not spawn in a physics object
    Made manual biomes
    Added TOO MANY DICTIONARIES

--- Insanifactor GD/Scripts/Main.gd
+++ Insanifactor GD/Scripts/Main.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 # Chunk radius
-var chunkRadius = 2;
+var chunkRadius = 4;
+
+# Spawn area for clearing obsticles (it's the radius so to say, but still a square area)
+var spawnAreaSize = Vector2i(5, 5);
 
 # Get player coords to generate chunks
 @onready var player = get_node("/root/Main/Player");
@@ -9,15 +12,28 @@ var chunkRadius = 2;
 @onready var camera = get_node("/root/Main/Player/Camera");
 
 @onready var terrain = get_node("/root/Main/Terrain");
-func _physics_process(delta):
+
+func _ready():
+	# Gen terrain so we can clear it
+	generatePlayer(player);
 	
+	# Make the boundries, and clear the area of spawn
+	var topLeft = terrain.getPositionToTile(player.position) - spawnAreaSize;
+	var bottomRight = terrain.getPositionToTile(player.position) + spawnAreaSize;
+	terrain.fillArea(topLeft, bottomRight, terrain.TileExact.CLEAR_FLOOR);
+
+func _physics_process(delta):
+	generatePlayer(player);
+	
+
+func generatePlayer(player):
 	# Generate in a radius of player offset by player width and height
 	var playerWidth = 32;
 	var playerHeight = 32;
 	
 	terrain.setChunkRadius(chunkRadius);
 	
-	#  Vector2(terrain.chunkSizeX * 32 / 2, terrain.chunkSizeY * 32 / 2) means add 2/1 tiles to the player pos
+	#  Vector2(terrain.chunkSizeX * 32 / 2, terrain.chunkSizeY * 32 / 2) means add (2, 1) tiles to the player pos
 	var playerPos = terrain.getPositionToTile(player.position + Vector2(playerWidth / 2, playerHeight / 2) + Vector2(terrain.chunkSizeX * 32 + 32, terrain.chunkSizeY * 32 / 2));
 	
 	
@@ -25,7 +41,6 @@ func _physics_process(delta):
 	print(playerPos);
 	# Generate chunks at player
 	terrain.generatePlayer(playerPos);
-
 
 func onChunkRadiusChanged(tempChunkRadius):
 	chunkRadius = tempChunkRadius;
--- Insanifactor GD/Scripts/TileMap.gd
+++ Insanifactor GD/Scripts/TileMap.gd
@@ -1,6 +1,8 @@
 extends TileMap
 
-
+# Some info: Always setcell at layer 0, because that is the floor layer
+# Current tilemap:
+var tileMapIndex = 1;
 # Tile stuff
 
 # Types of tile
@@ -8,7 +10,7 @@ enum TileType{
 	WALL,
 	WATER,
 	AIR
-}
+};
 enum TileExact{
 	AIR,
 	CLEAR_FLOOR,
@@ -16,18 +18,49 @@ enum TileExact{
 	WATER_MEDIUM,
 	WATER_DEEP,
 	WALL_ALONE,
+	WALL_ALONE_COLLIDE,
+	WALL_MIDDLE,
 	WALL_LEFT,
 	WALL_RIGHT,
 	WALL_TOP,
-	WALL_DOWN,
+	WALL_BOTTOM,
 	WALL_TOP_LEFT,
 	WALL_TOP_RIGHT,
 	WALL_BOTTOM_LEFT,
 	WALL_BOTTOM_RIGHT
-}
+};
 
+# Dictionaries for each tile about their type and exact type
 var tileType = {};
 var tileExact = {};
+
+# Conversion table for turning tileExacts to tileTypes
+var tileTypeTable = {
+	TileExact.AIR :                TileType.AIR,
+	TileExact.CLEAR_FLOOR :        TileType.AIR,
+	TileExact.WATER_SHALLOW :      TileType.WATER,
+	TileExact.WATER_MEDIUM :       TileType.WATER,
+	TileExact.WATER_DEEP :         TileType.WATER,
+	TileExact.WALL_ALONE :         TileType.WALL,
+	TileExact.WALL_ALONE_COLLIDE : TileType.WALL,
+	TileExact.WALL_TOP_LEFT :      TileType.WALL,
+	TileExact.WALL_TOP :           TileType.WALL,
+	TileExact.WALL_TOP_RIGHT :     TileType.WALL,
+	TileExact.WALL_LEFT :          TileType.WALL,
+	TileExact.WALL_MIDDLE :        TileType.WALL,
+	TileExact.WALL_RIGHT :         TileType.WALL,
+	TileExact.WALL_BOTTOM_LEFT :   TileType.WALL,
+	TileExact.WALL_BOTTOM :        TileType.WALL,
+	TileExact.WALL_BOTTOM_RIGHT :  TileType.WALL,
+};
+
+# Tile textures in dictionary to easily and fastly recieve texture atlas coords
+var tileTextures = {
+	TileExact.AIR : Vector2i(0, -1),
+	TileExact.CLEAR_FLOOR : Vector2i(0, 0),
+	TileExact.WALL_ALONE : Vector2i(0, 1),
+	TileExact.WALL_ALONE_COLLIDE : Vector2i(0, 2)
+};
 
 # Size of chunks (in tiles)
 var chunkSizeX = 2;
@@ -39,15 +72,17 @@ var chunkRadius = 5;
 var generatedChunks = {};
 
 # Biomes
-
 enum BiomeType{
-	
+	VOID,
+	NORMAL
 }
 
 var biomeType = {};
+var biomePercentage = {};
 # Perlin noise maps
 var biome = FastNoiseLite.new();
 var altitude = FastNoiseLite.new();
+
 
 func _ready():
 	# Randomize the noise
@@ -61,11 +96,13 @@ func _process(delta):
 
 func generatePlayer(tilePos : Vector2i):
 	generateRadius(chunkRadius, tilePos);
-	# Divide tilepos by 2 because it is to fast for some reason
+	# Divide tilepos by 2 because it is too fast for some reason
 	setPhysicsRadius(chunkRadius * min(chunkSizeX, chunkSizeY) - 1, tilePos / 2);
 
 func generateRadius(radius : int, tilePos : Vector2i):
 	var chunkPos = convertToChunkpos(tilePos);
+	
+	# (Technically this generates in a square, but it's around the player)
 	
 	# For every tile in a radius
 	# (chunkRadius is (center - radius) + 1 to (center + radius) - 1)
@@ -85,26 +122,32 @@ func generateChunk(chunkPos : Vector2i):
 	tilePos.y = chunkPos.y * chunkSizeY - chunkSizeY;
 	
 	# Generate from 0 to chunkSize
-	for x in range(chunkSizeX):
-		for y in range(chunkSizeY):
-			var alt = altitude.get_noise_2d(tilePos.x + x, tilePos.y + y);
+	for tileX in range(chunkSizeX):
+		for tileY in range(chunkSizeY):
+			var alt = altitude.get_noise_2d(tilePos.x + tileX, tilePos.y + tileY);
 			
-			# If the altitude at a point is more than 1, set tile to wall 
-			if(floor(alt + 1) > 0):
-				# Set wall
-				set_cell(0, Vector2i(tilePos.x + x, tilePos.y + y), 1, Vector2i(0, 1));
-				# Add wall to tileType array
-				tileType[Vector2i(tilePos.x + x, tilePos.y + y)] = TileType.WALL;
-				tileExact[Vector2i(tilePos.x + x, tilePos.y + y)] = TileExact.WALL_ALONE;
+			# bio is from 0 to 1, get_noise_2d goes from -0.5 to 0.5
+			var bio = biome.get_noise_2d(tilePos.x + tileX, tilePos.y + tileY) + .5;
+			print(bio);
+			# If biome variable is less than .9, than the biome will be normal
+			if(bio < .9):
+				biomeType[tilePos + Vector2i(tileX, tileY)] = BiomeType.NORMAL;
 			else:
-				# Set floor
-				set_cell(0, Vector2i(tilePos.x + x, tilePos.y + y), 1, Vector2i(0, 0));
-				# If floor, it is air
-				tileType[Vector2i(tilePos.x + x, tilePos.y + y)] = TileType.AIR;
-				tileExact[Vector2i(tilePos.x + x, tilePos.y + y)] = TileExact.CLEAR_FLOOR;
-
-func fillArea(tilePos1 : Vector2i, tilePos2 : Vector2i, Tile):
-	pass
+				biomeType[tilePos + Vector2i(tileX, tileY)] = BiomeType.VOID;
+			
+			# Normal biome type
+			if(biomeType[tilePos + Vector2i(tileX, tileY)] == BiomeType.NORMAL):
+				# If the altitude at a point is more than 1, set tile to wall 
+				if(floor(alt + 1) > 0):
+					# Set wall
+					setTile(Vector2i(tilePos + Vector2i(tileX, tileY)), TileExact.WALL_ALONE);
+				else:
+					# Set floor
+					setTile(Vector2i(tilePos + Vector2i(tileX, tileY)), TileExact.CLEAR_FLOOR);
+			
+			# Void biome
+			elif(biomeType[tilePos + Vector2i(tileX, tileY)] == BiomeType.VOID):
+				setTile(Vector2i(tilePos + Vector2i(tileX, tileY)), TileExact.AIR);
 
 func setPhysicsRadius(radius : int, tilePos : Vector2i):
 	# For every tile in a radius
@@ -142,9 +185,28 @@ func convertToChunkpos(tilePosition : Vector2i):
 	chunkPos.y = floor(tilePosition.y / chunkSizeY);
 	return chunkPos;
 
-func setChunkRadius(tempChunkRadius : int):
-	chunkRadius = tempChunkRadius;
-
 func getPositionToTile(position : Vector2):
 	# Divide the position by the scale of this object
 	return local_to_map(position / scale);
+
+func setChunkRadius(tempChunkRadius : int):
+	chunkRadius = tempChunkRadius;
+
+func fillArea(tilePos1 : Vector2i, tilePos2 : Vector2i, TileExact):
+	for tileX in range(tilePos1.x, tilePos2.x):
+		for tileY in range(tilePos1.y, tilePos2.y):
+			setTile(Vector2i(tileX, tileY), TileExact)
+
+func setTile(tilePos : Vector2i, TileExact):
+	# TileExact != tileExact, tileExact = enum while TileExact in this function is a pointer to a specific enum inside tileExact.
+	
+	# Decifering this is, set cell at (tilePos) the sprite at (vec2i) of spritesheet (tileMapIndex)
+	set_cell(0, tilePos, tileMapIndex, tileTextures.get(TileExact));
+	
+	# Set the TileExact and TileType
+	
+	# At tilepos set tileExact to TileExact
+	tileExact[tilePos] = TileExact;
+	
+	# At tilepos set the TileType to convertToTileType(TileExact)
+	tileType[tilePos] = tileTypeTable.get(TileExact);
